ad83f4c091510c41b8b50fea58d311728f9ef7d7
johnfelipe <ingenierofelipeurrego@gmail.com>
2023-05-08 13:24:00 -0500

    1st commit

---
+++ puzzle/easy/1d-spreadsheet.py
@@ -0,0 +1,71 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+class SpreadSheet:
+    def __init__(self):
+        self.cells = []
+
+    def get_cell(self, index):
+        return self.cells[index]
+
+    def add_cell(self, cell):
+        self.cells.append(cell)
+        cell.spread_sheet = self
+
+
+class Cell:
+    def __init__(self, operation, arg1, arg2):
+        self.operation = operation
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.spread_sheet = None
+        self.value = None
+
+    def get_arg_value(self, arg):
+        if '$' in arg:
+            return self.spread_sheet.get_cell(int(arg[1:])).get_value()
+        elif arg == '_':
+            return '0'
+        else:
+            return arg
+
+    def get_value(self):
+        return self.value if self.value is not None else self.calc_value()
+
+    def calc_value(self):
+        arg1 = int(self.get_arg_value(self.arg1))
+        arg2 = int(self.get_arg_value(self.arg2))
+
+        result = 0
+        if self.operation == 'VALUE':
+            result = arg1
+        elif self.operation == 'ADD':
+            result = arg1 + arg2
+        elif self.operation == 'SUB':
+            result = arg1 - arg2
+        elif self.operation == 'MULT':
+            result = arg1 * arg2
+
+        self.value = str(result)
+
+        return self.value
+
+
+n = int(input())
+spread_sheet = SpreadSheet()
+for i in range(n):
+    operation, arg_1, arg_2 = input().split()
+
+    spread_sheet.add_cell(Cell(operation, arg_1, arg_2))
+
+for i in range(n):
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    # print("1")
+    print(spread_sheet.get_cell(i).get_value())
---
+++ puzzle/easy/walk-on-a-die.py
@@ -0,0 +1,96 @@
+# import sys
+# import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+lines = []
+for i in range(3):
+    line = input()  # One line out of three in the string describing the starting position.
+    lines.append(line)
+commands = input()  # The sequence of ULDR-characters describing the steps to perform.
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+class Dice:
+    def __init__(self, numbers_string):
+        self.front, self.left, self.on, self.right, self.opposite, self.behind = list(numbers_string)
+
+    def move(self, commands):
+        command_to_method = {
+            'U': self.move_to_forward,
+            'D': self.move_to_backward,
+            'L': self.move_to_left,
+            'R': self.move_to_right
+        }
+
+        for command in commands:
+            command_to_method[command]()
+
+    def move_to_forward(self):
+        temp_opposite = self.opposite
+        temp_front = self.front
+        temp_behind = self.behind
+        temp_on = self.on
+
+        self.front = temp_opposite
+        self.on = temp_front
+        self.opposite = temp_behind
+        self.behind = temp_on
+
+    def move_to_backward(self):
+        temp_opposite = self.opposite
+        temp_right = self.right
+        temp_behind = self.behind
+        temp_left = self.left
+        temp_front = self.front
+        temp_on = self.on
+
+        self.front = temp_opposite
+        self.left = temp_right
+        self.on = temp_behind
+        self.right = temp_left
+        self.opposite = temp_front
+        self.behind = temp_on
+
+    def move_to_left(self):
+        temp_opposite = self.opposite
+        temp_behind = self.behind
+        temp_left = self.left
+        temp_front = self.front
+        temp_right = self.right
+        temp_on = self.on
+
+        self.front = temp_opposite
+        self.left = temp_behind
+        self.on = temp_left
+        self.right = temp_front
+        self.opposite = temp_right
+        self.behind = temp_on
+
+    def move_to_right(self):
+        temp_opposite = self.opposite
+        temp_front = self.front
+        temp_right = self.right
+        temp_behind = self.behind
+        temp_left = self.left
+        temp_on = self.on
+
+        self.front = temp_opposite
+        self.left = temp_front
+        self.on = temp_right
+        self.right = temp_behind
+        self.opposite = temp_left
+        self.behind = temp_on
+
+
+dice = Dice(''.join(lines).replace(' ', ''))
+dice.move(commands)
+
+result = dice.on
+
+# The number on the side you end up on after having performed `commands`.
+# print("number")
+print(result)
